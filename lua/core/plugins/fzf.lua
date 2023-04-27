9d2a7d2191ddf88baadfa4363246cf9ed780b58c
carlosmera24 <carlosemr24@yahoo.com.ar>
2023-04-27 12:43:28 -0500

    Ajustados comandos de activcación del plugin para FZF (:Rg, :FZF)

--- lua/core/plugins/fzf.lua
+++ lua/core/plugins/fzf.lua
@@ -7,6 +7,7 @@ return {
             { "<leader>t", ":FZF<CR>" },
             -- Abrir Rg busqueda con leader+F (leader+Shift+f)
             { "<leader>F", ':Rg<CR>' }
-        }
+        },
+        cmd = { "Rg", "FZF" }
     }
 }
